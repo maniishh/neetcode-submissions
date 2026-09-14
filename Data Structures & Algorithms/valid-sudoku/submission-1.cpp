b3e0838c9d60578af0b6ccdb9fefc407f5059ce7
Manish Kumar Yadav <162919290+maniishh@users.noreply.github.com>
2026-09-14 09:12:46 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.cpp
@@ -0,0 +1,50 @@
+class Solution {
+   private:
+    bool valid(vector<vector<char>>& board, int sr, int er, int sc, int ec) {
+        unordered_set<int> st;
+        for (int i = sr; i <= er; i++) {
+            for (int j = sc; j <= ec; j++) {
+                if (board[i][j] == '.') continue;
+                if (st.find(board[i][j]) != st.end()) {
+                    return false;
+                }
+                st.insert(board[i][j]);
+            }
+        }
+        return true;
+    }
+
+   public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        
+        // check for rows
+        for (int i = 0; i < 9; i++) {
+            unordered_set<int> st;
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+                if (st.find(board[i][j]) != st.end()) return false;
+                st.insert(board[i][j]);
+            }
+        }
+        // check for cols
+        for (int i = 0; i < 9; i++) {
+            unordered_set<int> st;
+            for (int j = 0; j < 9; j++) {
+                if (board[j][i] == '.') continue;
+                if (st.find(board[j][i]) != st.end()) return false;
+                st.insert(board[j][i]);
+            }
+        }
+        // check for boxes
+        for (int sr = 0; sr < 9; sr += 3) {
+            int er = sr + 2;
+            for (int sc = 0; sc < 9; sc += 3) {
+                int ec = sc + 2;
+                if (!valid(board, sr, er, sc, ec)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
