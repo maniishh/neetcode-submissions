90b6e09567bbbb980529f3b694eb1aeb1f28ec0d
Manish Kumar Yadav <162919290+maniishh@users.noreply.github.com>
2026-09-25 11:35:20 +0530

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (!head || !head->next) return;
+
+        // 1. Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // 2. Reverse second half
+        ListNode* second = slow->next;
+        slow->next = nullptr;
+
+        ListNode* prev = nullptr;
+
+        while (second) {
+            ListNode* next = second->next;
+            second->next = prev;
+            prev = second;
+            second = next;
+        }
+
+        second = prev;
+
+        // 3. Merge alternately
+        ListNode* first = head;
+
+        while (second) {
+            ListNode* temp1 = first->next;
+            ListNode* temp2 = second->next;
+
+            first->next = second;
+            second->next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+    }
+};
