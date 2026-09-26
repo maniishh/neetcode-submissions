f27d79b956d0e2c6b2dd4eb8d10f67a6a1f073e2
Manish Kumar Yadav <162919290+maniishh@users.noreply.github.com>
2026-09-26 22:14:25 +0530

    Add: remove-node-from-end-of-linked-list - submission-1

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-1.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+   public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        // size is m,1 2 3 4
+        // from end is will 3   , from start  iit is 3rd node  (m-n+1)th node from start
+        // int m = 0;
+        // ListNode* temp = head;
+        // while (temp) {
+        //     temp = temp->next;
+        //     m++;
+        // }
+        // the size of ll is m
+        // m-n+1th node from the start
+        // temp = head;
+        // int x = m - n;
+        // if (m == n) return head->next;
+        // for (int i = 1; i < x; i++) {
+        //     temp = temp->next;
+        // }
+        // temp->next = temp->next->next;
+        // return head;
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* slow = dummy;
+        ListNode* fast = dummy;
+        for (int i = 0; i < n; i++) {
+            fast = fast->next;
+        }
+        while (fast->next) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        slow->next = slow->next->next;
+        return dummy->next;
+    }
+};
